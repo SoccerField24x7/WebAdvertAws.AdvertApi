81b9e2a8644958986c1c2dce2a766296c2934496
Jesse Quijano <jesse@quijano.net>
2021-05-23 20:04:56 -0700

    implement interface

---
+++ AdvertApi/Services/DynamoDBAdvertStorage.cs
@@ -0,0 +1,66 @@
+﻿using AdvertApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace AdvertApi.Services
+{
+    public class DynamoDBAdvertStorage : IAdvertStorageService
+    {
+        private readonly IMapper _mapper;
+
+        public DynamoDBAdvertStorage(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public IMapper Mapper { get; }
+
+        public async Task<string> Add(AdvertModel model)
+        {
+            // TODO: Add validation
+            var dbModel = _mapper.Map<AdvertDbModel>(model);
+            using (var client = new AmazonDynamoDBClient()) // because credentials in OS and we have appsettings, no params needed
+            {
+                dbModel.Id = new Guid().ToString();
+                dbModel.CreationDateTime = DateTime.UtcNow;
+                dbModel.Status = AdvertStatus.Pending;
+
+                using (var context = new DynamoDBContext(client))
+                {
+                    await context.SaveAsync(dbModel);
+                }
+            }
+
+            return dbModel.Id;
+        }
+
+        public async Task Confirm(ConfirmAdvertModel model)
+        {
+            using (var client = new AmazonDynamoDBClient())
+            {
+                using (var context = new DynamoDBContext(client))
+                {
+                    var record = await context.LoadAsync<AdvertDbModel>(model.Id);
+                    if (record == null)
+                    {
+                        throw new KeyNotFoundException($"A record with ID={model.Id} was not found.");
+                    }
+                    if (model.Status == AdvertStatus.Active)
+                    {
+                        record.Status = AdvertStatus.Active;
+                        await context.SaveAsync(record);
+                    }
+                    else
+                    {
+                        await context.DeleteAsync(record);
+                    }
+                }
+            }
+        }
+    }
+}
